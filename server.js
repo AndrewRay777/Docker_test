310442552177ce6b62a826aeb298d5d9a94d8910
AndrewRay777 <andreyfeoktistov7777@gmail.com>
2026-05-23 01:14:33 +0300

    update: change welcome text

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const app = express();
 const PORT = 3000;
 
 app.get('/', (req, res) => {
-    res.send('<h1>🚀 CI/CD в Docker на Windows успешно работает! 🚀</h1>');
+    res.send('<h1>🚀 CI/CD в Docker на Windows успешно работает! https://www.youtube.com/ 🚀</h1>');
 });
 
 app.listen(PORT, () => {
